bace7499ceb383b60ea17cc5c8b7087ac2708178
macbook <macbook@macbooks-MBP.lan>
2024-02-16 11:46:06 -0800

    updates scroll behavior

--- src/components/cardContainer/CardContainer.jsx
+++ src/components/cardContainer/CardContainer.jsx
@@ -1,10 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Card from '../card/Card'; 
 import './CardContainer.scss';
 
 function CardContainer({ items, openModal }) {
   const scrollContainer = useRef(null);
+  const [showLeftScroll, setShowLeftScroll] = useState(false);
   const cardWidth = 395; 
+
+  const checkScrollPosition = () => {
+    if (scrollContainer.current.scrollLeft > 0) {
+      setShowLeftScroll(true);
+    } else {
+      setShowLeftScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    checkScrollPosition();
+    scrollContainer.current.addEventListener('scroll', checkScrollPosition);
+    return () => {
+      scrollContainer.current.removeEventListener('scroll', checkScrollPosition);
+    };
+  }, []);
 
   const scrollLeft = () => {
     scrollContainer.current.scrollTo({
@@ -21,15 +38,15 @@ function CardContainer({ items, openModal }) {
   };
 
   return (
-<div className="card-container-outer">
-  <button onClick={scrollLeft}></button>
-  <div className="card-container-inner" ref={scrollContainer}>
-    {items.map((item, index) => (
-      <Card key={index} item={item} openModal={openModal} />
-    ))}
-  </div>
-  <button onClick={scrollRight}></button>
-</div>
+    <div className="card-container-outer">
+      {showLeftScroll && <button onClick={scrollLeft}></button>}
+      <div className="card-container-inner" ref={scrollContainer}>
+        {items.map((item, index) => (
+          <Card key={index} item={item} openModal={openModal} />
+        ))}
+      </div>
+      {items.length > 3 && <button onClick={scrollRight}></button>}
+    </div>
   );
 }
 
