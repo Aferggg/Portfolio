02c5283a315648d7b2fe7e44ba8b410cad71a93d
macbook <macbook@macbooks-MBP.lan>
2024-03-29 11:21:42 -0700

    intro interactivity update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./intro.scss"
+import introbg from "../../images/introbg.png"
+import introbg2 from "../../images/introbg2.png"
+import introbg3 from "../../images/introbg3.png"
+import introbg4 from "../../images/introbg4.png"
+import introbg5 from "../../images/introbg5.png"
+import introbg6 from "../../images/introbg6.png"
 
 export default function Intro() {
-    
+    const images = [introbg, introbg2, introbg3, introbg4, introbg5, introbg6];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handleClick = () => {
+        setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    };
 
     return (
-        <div className="intro" id="intro" 
-            >
+        <div className="intro" id="intro" onClick={handleClick} style={{backgroundImage: `url(${images[currentImageIndex]})`}}>
             <div className="imgContainer">
-                </div>
+                {/* ImgContainer content */}
+            </div>
         </div>
     )
 }
